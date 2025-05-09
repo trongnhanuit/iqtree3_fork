af8952843e150c2b848926fd0cf14018d20ae455
NhanLT <trongnhan.uit@gmail.com>
2025-05-09 16:29:47 +1000

    remove redundant ",," when outputting the best_model.nex caused by code merging

--- tree/phylosupertree.cpp
+++ tree/phylosupertree.cpp
@@ -1569,8 +1569,7 @@ void PhyloSuperTree::printBestPartitionParams(const char *filename) {
             if (!saln->partitions[part]->aln_file.empty()) out << saln->partitions[part]->aln_file << ": ";
             /*if (saln->partitions[part]->seq_type == SEQ_CODON)
                 out << "CODON, ";*/
-            if (!saln->partitions[part]->sequence_type.empty())
-                out << saln->partitions[part]->sequence_type << ",";
+            out << saln->partitions[part]->sequence_type;
             string pos = saln->partitions[part]->position_spec;
             replace(pos.begin(), pos.end(), ',' , ' ');
             if (!saln->partitions[part]->sequence_type.empty() && !pos.empty()) {

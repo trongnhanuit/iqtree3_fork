cae233b8f91413bfcbe965678027e7a022a38561
NhanLT <trongnhan.uit@gmail.com>
2025-07-07 17:27:30 +1000

    add missing log in a special case - don't need to infer gaps in non-gapped alignments

--- main/phyloanalysis.cpp
+++ main/phyloanalysis.cpp
@@ -5332,6 +5332,9 @@ IQTree* reconstructGappedSeqs(Params params, IQTree* original_tree)
     {
         delete alignment;
         
+        if (verbose_mode >= VB_MIN)
+            cout << "----- Finish reconstructing gapped sequences -----" << endl;
+        
         return nullptr;
     }
     

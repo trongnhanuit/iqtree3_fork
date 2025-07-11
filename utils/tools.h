d3595775553d433d67b6dad5fb0a0c7cc42ea492
NhanLT <trongnhan.uit@gmail.com>
2025-07-11 14:30:01 +1000

    support non-reversible models for gapped-ASR/ESR with partitions

--- utils/tools.h
+++ utils/tools.h
@@ -2030,6 +2030,9 @@ public:
     
     /** TRUE to reconstruct (ancestral/extant) sequences with gap prediction */
     bool gapped_seq_reconstruction;
+    
+    /** TRUE to allow nonrev (UNREST) models for binary data */
+    bool allow_nonrev_bin;
 
     /**
         0: print nothing
